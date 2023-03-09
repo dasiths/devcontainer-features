ac94b38ff88dbc513cbe495c93e6dfffe438223c
Dasith Wijes <dasith.wijes@microsoft.com>
2023-03-10 10:53:46 +1100

    run script from remote url

--- src/grype/install.sh
+++ src/grype/install.sh
@@ -6,6 +6,5 @@ echo "Installing Grype"
 ## adding grype
 RELEASE_VERSION=${VERSION:-"latest"}
 DESTINATION_DIR=${DESTINATION_DIR:-"/usr/local/bin"}
-chmod +x install-grype.sh
-sudo sh install-grype.sh -s -- -b $DESTINATION_DIR $RELEASE_VERSION
+curl -sSfL https://raw.githubusercontent.com/anchore/grype/main/install.sh | sudo sh -s -- -b $DESTINATION_DIR $RELEASE_VERSION
 grype version
